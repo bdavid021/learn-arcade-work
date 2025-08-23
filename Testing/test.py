869b8c3d810ec93f673eb90e2439a8777092fd3e
bdavid021 <davidonetab4@gmail.com>
2025-08-23 14:25:56 +0300

    saved

--- Testing/test.py
+++ Testing/test.py
@@ -1,3 +1,20 @@
-print("It was a dark and stormy night.")
-print("Suddenly a shot rang out!")
-print("hei \"mister'")
+import random
+import arcade
+
+numar = random.randint(1, 100)  # include atât 1 cât și 100
+print(numar)
+
+
+
+
+for i in range(5):
+    raspuns = int(input("La ce numar ma gandesc? "))
+    if raspuns == numar:
+        print("Felicitari ai gasit numarul la care ma gandesc!")
+
+    elif raspuns < numar:
+        print("Numarul la care ma gandesc e mai mare")
+    elif raspuns > numar:
+        print("Numarul la care ma gandesc e mai mic")
+
+print("ai ramas fara vieti")
